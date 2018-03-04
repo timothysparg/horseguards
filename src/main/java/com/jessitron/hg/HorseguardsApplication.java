ab79c8b1b00db2290800e414d0fa0c0344de1222
Jessica Kerr <jessitron@gmail.com>
2018-03-04 02:14:25 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/jessitron/hg/HorseguardsApplication.java
@@ -0,0 +1,12 @@
+package com.jessitron.hg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseguardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseguardsApplication.class, args);
+	}
+}
