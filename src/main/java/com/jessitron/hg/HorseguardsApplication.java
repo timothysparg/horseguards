ba77684524cdd41441097aeeb40ea117a2fd240d
Jessica Kerr <jessitron@gmail.com>
2018-03-04 17:02:24 +0000

    commit it all right now

--- src/main/java/com/jessitron/hg/HorseguardsApplication.java
+++ src/main/java/com/jessitron/hg/HorseguardsApplication.java
@@ -1,12 +1,12 @@
-package com.jessitron.hg;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class HorseguardsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HorseguardsApplication.class, args);
-	}
-}
+package com.jessitron.hg; // and kitties
+ // and kitties
+import org.springframework.boot.SpringApplication; // and kitties
+import org.springframework.boot.autoconfigure.SpringBootApplication; // and kitties
+ // and kitties
+@SpringBootApplication // and kitties
+public class HorseguardsApplication { // and kitties
+ // and kitties
+	public static void main(String[] args) { // and kitties
+		SpringApplication.run(HorseguardsApplication.class, args); // and kitties
+	} // and kitties
+} // and kitties
