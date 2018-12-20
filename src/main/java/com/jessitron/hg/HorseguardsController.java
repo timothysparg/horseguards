0c558c4ca1e6e05cad802c4dffef0b83945e8b15
Jessica Kerr <jessitron@atomist.com>
2018-12-20 13:47:38 -0600

    Fewer kitties

--- src/main/java/com/jessitron/hg/HorseguardsController.java
+++ src/main/java/com/jessitron/hg/HorseguardsController.java
@@ -1,27 +1,27 @@
-package com.jessitron.hg; // and kitties
- // and kitties
-import org.springframework.web.bind.annotation.PathVariable; // and kitties
-import org.springframework.web.bind.annotation.RequestMapping; // and kitties
-import org.springframework.web.bind.annotation.RestController; // and kitties
- // and kitties
-import static org.springframework.web.bind.annotation.RequestMethod.GET; // and kitties
- // and kitties
-@RestController // and kitties
-class HorseguardsController { // and kitties
- // and kitties
-    @RequestMapping(method = GET, path = "/") // and kitties
-    public String root() { // and kitties
-        return "App running: Served from " + getClass().getName(); // and kitties
-    } // and kitties
- // and kitties
-    @RequestMapping(method = GET, path = "hello/{name}") // and kitties
-    public String person(@PathVariable String name) { // and kitties
-        return "Hello " + name + "!"; // and kitties
-    } // and kitties
- // and kitties
-    @RequestMapping(method = GET, path = "kitties/{qyt}") // and kitties
-    public String kitties(@PathVariable Integer qty) { // and kitties
-        return "So many kitties" + qty; // and kitties
-    } // and kitties
- // and kitties
-} // and kitties
+package com.jessitron.hg;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+class HorseguardsController {
+
+    @RequestMapping(method = GET, path = "/")
+    public String root() {
+        return "App running: Served from " + getClass().getName();
+    }
+
+    @RequestMapping(method = GET, path = "hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    } 
+
+    @RequestMapping(method = GET, path = "kitties/{qyt}")
+    public String kitties(@PathVariable Integer qty) {
+        return "So many kitties" + qty;
+    }
+
+}
--- src/test/java/com/jessitron/hg/HorseguardsApplicationTests.java
+++ src/test/java/com/jessitron/hg/HorseguardsApplicationTests.java
@@ -1,16 +1,16 @@
-package com.jessitron.hg; // and kitties
- // and kitties
-import org.junit.Test; // and kitties
-import org.junit.runner.RunWith; // and kitties
-import org.springframework.boot.test.context.SpringBootTest; // and kitties
-import org.springframework.test.context.junit4.SpringRunner; // and kitties
- // and kitties
-@RunWith(SpringRunner.class) // and kitties
-@SpringBootTest // and kitties
-public class HorseguardsApplicationTests { // and kitties
- // and kitties
-	@Test // and kitties
-	public void contextLoads() { // and kitties
-	} // and kitties
- // and kitties
-} // and kitties
+package com.jessitron.hg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HorseguardsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
