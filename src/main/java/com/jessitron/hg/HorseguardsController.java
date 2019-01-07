284e0c61201860ef39f94a08442227d483a8462a
Jessica Kerr <jessitron@atomist.com>
2019-01-07 11:11:24 -0600

    Simplify controller

--- src/main/java/com/jessitron/hg/HorseguardsController.java
+++ src/main/java/com/jessitron/hg/HorseguardsController.java
@@ -1,18 +1,11 @@
 package com.jessitron.hg;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import sun.net.www.http.HttpClient;
 
 import java.io.IOException;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@RestController
 class HorseguardsController {
 
-    @RequestMapping(method = GET, path = "/")
     public String root() throws IOException {
 
         HorseguardsClient client = new HorseguardsClient();
@@ -21,16 +14,6 @@ class HorseguardsController {
             .execute();
 
         return "App running: Served from " + getClass().getName();
-    }
-
-    @RequestMapping(method = GET, path = "hello/{name}")
-    public String person(@PathVariable String name) {
-        return "Hello " + name + "!";
-    }
-
-    @RequestMapping(method = GET, path = "kitties/{qyt}")
-    public String kitties(@PathVariable Integer qty) {
-        return "So many kitties" + qty;
     }
 
 }
