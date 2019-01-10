9d24cc3c52ee170f05757a936e2f8f137c6493ea
Jessica Kerr <jessitron@atomist.com>
2019-01-10 11:35:48 -0600

    Make it a method

--- src/main/java/com/jessitron/hg/HorseguardsController.java
+++ src/main/java/com/jessitron/hg/HorseguardsController.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 class HorseguardsController {
 
-    public String root() throws IOException {
+    public String storingResponse() throws IOException {
 
         HorseguardsClient client = new HorseguardsClient();
 
@@ -14,7 +14,19 @@ class HorseguardsController {
             .execute();
 
         return "App running: Served from " + getClass().getName() +
-                " got " + response.statusCode;
+                " got " + response.statusCode();
+    }
+
+    public String storingStatusCode() throws IOException {
+
+        HorseguardsClient client = new HorseguardsClient();
+
+        int statusCode = client.get("https://bananas.com")
+                .execute()
+                .statusCode();
+
+        return "App running: Served from " + getClass().getName() +
+                " got " + statusCode;
     }
 
 }
--- src/main/java/com/jessitron/hg/HorseguardsResponse.java
+++ src/main/java/com/jessitron/hg/HorseguardsResponse.java
@@ -2,9 +2,13 @@ package com.jessitron.hg;
 
 public class HorseguardsResponse {
 
-    public final int statusCode;
+    private final int statusCode;
 
     public HorseguardsResponse(int statusCode) {
         this.statusCode = statusCode;
+    }
+
+    public int statusCode() {
+        return statusCode;
     }
 }
