a1eb01d90dbab4f89b747106101eac78c4bc6361
Jessica Kerr <jessitron@atomist.com>
2019-01-17 13:56:30 -0800

    Illustrate that it doesn't change it on other classes

---
+++ src/main/java/com/jessitron/hg/RenameMethod/AnotherClassWithOldMethod.java
@@ -0,0 +1,8 @@
+package com.jessitron.hg.RenameMethod;
+
+public class AnotherClassWithOldMethod {
+
+    public String oldMethodName() {
+        return "I am not deprecated, I'm on some other class.";
+    }
+}
--- src/main/java/com/jessitron/hg/RenameMethod/CallerOfRenamedMethod.java
+++ src/main/java/com/jessitron/hg/RenameMethod/CallerOfRenamedMethod.java
@@ -2,9 +2,10 @@ package com.jessitron.hg.RenameMethod;
 
 public class CallerOfRenamedMethod {
 
-    public void doTheThing(DefinerOfRenamedMethod corm) {
+    public void doTheThing(AnotherClassWithOldMethod acwom, DefinerOfRenamedMethod corm) {
         // blah blah
 
         System.out.println("Here is the result: " + corm.oldMethodName());
+        System.out.println("Here is another result: " + acwom.oldMethodName());
     }
 }
