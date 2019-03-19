59a2f4fa10b51cee41c35651e4d8c08f0ac1a1f4
Jessica Kerr <jessitron@atomist.com>
2019-03-18 20:59:37 -0500

    Example parameters

--- src/main/java/com/jessitron/hg/HorseguardsApplication.java
+++ src/main/java/com/jessitron/hg/HorseguardsApplication.java
@@ -3,7 +3,7 @@ package com.jessitron.hg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {}, excludeName = { "exclude" })
 public class HorseguardsApplication {
 
 	public static void main(String[] args) {
